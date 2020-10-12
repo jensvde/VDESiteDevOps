1a54eb220a7e48e4df9ca7f2f508b90e34fce21f
root <root@instance-1.europe-west1-b.c.diesel-charge-291115.internal>
2020-10-12 01:06:55 +0000

    m

--- update_site.sh
+++ update_site.sh
@@ -8,6 +8,7 @@ GITLINK="https://github.com/jensvde/VDESite.git /home/$USERNAME/$GITPREFIX"
 sudo killall dotnet
 rm -r /home/$USERNAME/$GITPREFIX
 rm -r /home/$USERNAME/publish
+cd /home/$USERNAME
 git clone $GITLINK
 cp -avrfn /home/$USERNAME/$GITPREFIX/$APPNAME/$APPNAME/bin/Release/netcoreapp3.1/publish /home/$USERNAME/publish
 mv /home/$USERNAME/publish/$APPNAME.dll /home/$USERNAME/publish/$APPNAME.dll
